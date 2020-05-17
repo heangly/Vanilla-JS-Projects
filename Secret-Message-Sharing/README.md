b74ab26197728f83ef31e7db7a8c4a981bbcb2a3
Heang Ly <heanglypa@gmail.com>
2020-05-17 08:59:38 -0400

    secret message

---
+++ Secret-Message-Sharing/README.md
@@ -0,0 +1,12 @@
+Author: Heang Ly
+- Application: Secret Message Sharing
+- Date: 2020
+
+What does it do?
+- Taking user input and generating a URL for user to share the link of the message
+- Encoding message using Base64
+- Original message will be shown only if the secrete link is clicked
+
+What are technologies?
+- Vanilla Js
+- Materialize CSS 
---
+++ Secret-Message-Sharing/index.js
@@ -0,0 +1,28 @@
+// Example of Base64 Conversion:
+// btoa('sec') --> 'c2Vj'
+// atob('c2Vj') --> 'sec'
+
+const {hash} = window.location;
+const message = atob(hash.replace('#', ''));
+
+if (message){
+  document.querySelector('#message-form').classList.add('hide');
+  document.querySelector('#message-show').classList.remove('hide');
+
+  document.querySelector('h1').innerText = message;
+}
+
+document.querySelector('form').addEventListener('submit', e =>{
+  e.preventDefault();
+  
+  document.querySelector('#message-form').classList.add('hide');
+  document.querySelector('#link-form').classList.remove('hide');
+
+  const input = document.querySelector('#message-input');
+  const encrpted = btoa(input.value);
+
+  const linkInput = document.querySelector('#link-input');
+  linkInput.value = `${window.location}#${encrpted}`;
+  linkInput.select();
+
+});
